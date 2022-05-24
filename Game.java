4f92aa60c0aac337f9ffb21152d9d981019b25a2
Ifaz <Ifaz.aiman31@gmail.com>
2022-05-25 04:48:56 +0600

    Created a window

---
+++ Game.java
@@ -0,0 +1,10 @@
+
+class Game{
+    public static void main(String[] args){
+      Jframe nameOfFrmae =new Jframe;
+      frame.setsize(x,y);
+      frame.setdefaultcloseoperation(jframe.exit_on_close);
+      frame.setvisble(true);
+    }
+    
+    }
