461e3a669c0da40e4bc21e26e65c7807e03e17fa
Ifaz <Ifaz.aiman31@gmail.com>
2022-05-25 05:13:22 +0600

    Fixed Bug

--- GameFrame.java
+++ GameFrame.java
@@ -10,8 +10,7 @@ public class GameFrame extends JFrame{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setResizable(false);
 		this.pack();
-		this.setVisible(true);
 		this.setLocationRelativeTo(null);
-		
+		this.setVisible(true);
 	}
 }
