be5234f39a681585ec26ec4585007cd6474e8de9
Hridi <sadia80@student.sust.edu>
2022-08-15 23:22:47 +0600

    Snake is moving

--- src/com/ifazaiman/GamePanel.java
+++ src/com/ifazaiman/GamePanel.java
@@ -102,7 +102,12 @@ public class GamePanel extends JPanel implements ActionListener{
 	
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
+		if (running){
+			move();
+			checkApple();
+			checkCollisions();
+		}
+		repaint();
 		
 	}
 
