b42747949798f3ab3c19d0dfe65fc77bdb523784
Ifaz <Ifaz.aiman31@gmail.com>
2022-08-16 00:14:20 +0600

    Controlling the Snake Movement

--- src/com/ifazaiman/GamePanel.java
+++ src/com/ifazaiman/GamePanel.java
@@ -94,7 +94,31 @@ public class GamePanel extends JPanel implements ActionListener{
 		
 	}
 	public void checkCollisions() {
-		
+		//if head touches body
+		for(int i = bodyParts;i>0;i--) {
+			if((x[0] == x[i])&& (y[0] == y[i])) {
+				running = false;
+			}
+		}
+		//if head touches left border
+		if(x[0] < 0) {
+			running = false;
+		}
+		// if head touches right border
+		if(x[0] > SCREEN_WIDTH) {
+			running = false;
+		}
+		// if head touches top border
+		if(y[0] < 0) {
+			running = false;
+		}
+		// if head touches bottom border
+		if(y[0] > SCREEN_HEIGHT) {
+			running = false;
+		}
+		if(!running) {
+			timer.stop();
+		}
 	}
 	public void gameOver(Graphics g) {
 		
@@ -108,16 +132,34 @@ public class GamePanel extends JPanel implements ActionListener{
 			checkCollisions();
 		}
 		repaint();
-		
 	}
 
-	public static class MyKeyAdapter extends KeyAdapter{
+	public class MyKeyAdapter extends KeyAdapter{
 		@Override
 		public void keyPressed(KeyEvent e) {
-			
+			switch(e.getKeyCode()) {
+				case KeyEvent.VK_LEFT:
+					if(direction != 'R') {
+						direction = 'L';
+					}
+					break;
+				case KeyEvent.VK_RIGHT:
+					if(direction != 'L') {
+						direction = 'R';
+					}
+					break;
+				case KeyEvent.VK_UP:
+					if(direction != 'D') {
+						direction = 'U';
+					}
+					break;
+				case KeyEvent.VK_DOWN:
+					if(direction != 'U') {
+						direction = 'D';
+					}
+					break;
+			}
 		}
-		
 	}
-	
-	}
+}
 
